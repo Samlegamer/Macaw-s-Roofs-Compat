43004fabc58fa54494cadbdf717a58b5282a7315
Samlegamer <83614614+Samlegamer@users.noreply.github.com>
2022-02-18 22:09:53 +0100

    1.1 fix

---
+++ Compatibilities/Macaw's Roofs - Oh The Biomes You'll Go/src/main/java/fr/samlegamer/macawsroofsbyg/item/MRBYGItemsRegistry.java
@@ -0,0 +1,11 @@
+package fr.samlegamer.macawsroofsbyg.item;
+
+import fr.samlegamer.macawsroofsbyg.McwRoofsBYG;
+import net.minecraft.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class MRBYGItemsRegistry
+{
+	public static final DeferredRegister<Item> ITEMS_REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, McwRoofsBYG.MODID);
+}
