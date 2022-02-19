37faff47df4d33447fc47628f242a2c1c0a6e2d7
Samlegamer <83614614+Samlegamer@users.noreply.github.com>
2022-02-19 01:47:05 +0100

    Add files via upload

---
+++ Compatibilities/Macaw's Roofs - Biomes O' Plenty/src/main/java/fr/samlegamer/macawsroofsbop/item/MRBOPItemsRegistry.java
@@ -0,0 +1,11 @@
+package fr.samlegamer.macawsroofsbop.item;
+
+import fr.samlegamer.macawsroofsbop.MacawsRoofsBOP;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class MRBOPItemsRegistry
+{
+	public static final DeferredRegister<Item> ITEMS_REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, MacawsRoofsBOP.MODID);
+}
